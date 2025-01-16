db34582542133d0b41a3557b5e1594adc1007c5b
cda4 <mohamed.keita99@gmail.com>
2025-01-16 19:53:34 +0100

    fix 5

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,6 +3,29 @@ const { getUserTweetsFormAuthorId  } = require('../queries/tweets.queries');
 const multer = require('multer');
 const path = require('path');
 
+const upload = multer({ storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.join( __dirname, '../public/images/avatars'))
+        },
+        filename: (req, file, cb) => {
+            cb(null, `${ Date.now() }-${ file.originalname }`);
+        }
+    }) })
+
+
+exports.uploadImage = [
+    upload.single('avatar'),
+    async (req, res, next) => {
+        try {
+            const user = req.user;
+            user.avatar = `/images/avatars/${ req.file.filename }`;
+            await user.save();
+            res.redirect('/');
+        } catch(e) {
+            next(e);
+        }
+    }
+]
 
 exports.followUser = async (req, res, next) => {
     try {
@@ -49,14 +72,6 @@ exports.userProfile = async (req, res, next) => {
         next(e);
     }
 }
-const upload = multer({ storage: multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, path.join( __dirname, '../public/images/avatars'))
-        },
-        filename: (req, file, cb) => {
-            cb(null, `${ Date.now() }-${ file.originalname }`);
-        }
-    }) })
 
 
 exports.signupForm = (req, res, next) => {
@@ -80,17 +95,3 @@ exports.signup = async (req, res, next) => {
         });
     }
 }
-
-exports.uploadImage = [
-    upload.single('avatar'),
-    async (req, res, next) => {
-        try {
-            const user = req.user;
-            user.avatar = `/images/avatars/${ req.file.filename }`;
-            await user.save();
-            res.redirect('/');
-        } catch(e) {
-            next(e);
-        }
-    }
-]
